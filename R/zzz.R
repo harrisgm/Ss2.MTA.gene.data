8d5fde0dd4ae7fbd5e96bce82ccaa16e11b1fc1b
“Guy <harrisgm@vcu.edu>
2019-08-01 09:53:11 -0400

    commiting corrections

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,3 @@
 .onLoad <- function(libname, pkgname){
-  # pkgname <- "Ss2.MTA.gene.probedata"
-  packageStartupMessage("Sscore2 data files for the MTA gene-level analysis")
-  # data("Ss2MTAgene", package = pkgname)
-  # data(Ss2MTAgene, package = pkgname)
-  # data(list = pkgname, package = pkgname, envir = where)
-  # packageStartupMessage(AnnotationDbi:::annoStartupMessages(pkgname))
+  packageStartupMessage("contains Sscore2 data files for the MTA gene-level analysis")
 }
